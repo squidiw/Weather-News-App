85b8493a6c890f9c93dc995198c12710be4d614c
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-02-08 14:42:27 +0000

    Title & source for first 3 cards updates from api

--- js/news.js
+++ js/news.js
@@ -1,5 +1,6 @@
-const title = document.querySelector("h1.title"),
-   image = document.querySelectorAll(".image"),
+const title = document.querySelectorAll(".title"),
+   image = document.querySelectorAll("div.image"),
+   
    source = document.querySelectorAll("span.source"),
    url = document.querySelectorAll(".url"),
    newsList = document.querySelector(".band");
@@ -19,32 +20,31 @@ function getNews() {
          // console.log(data);
          let x = 0;
          data.articles.forEach(article => {
-            // title[x].innerHTML = article.title;
-            console.log(title);
-            // image[x].style.backgroundImage = article.image;
-            // source[x].innerHTML = article.source.name;
-            // url[x].setAttribute('href', article.url);
-            // url[x].setAttribute('target', '_blank');
-            // x < 8; x++;
+            title[x].innerHTML = article.title;
+            image[x].style.backgroundImage = article.image;
+            source[x].innerHTML = article.source.name;
+            url[x].setAttribute('href', article.url);
+            url[x].setAttribute('target', '_blank');
+             x++;
          });
 
-         data.articles.forEach((article, idx) => {
-            const content =`
+         // data.articles.forEach((article, idx) => {
+         //    const content =`
 
             
-               <div class="item-1 ${idx}">
-                  <a href=${article.url} class="card url">
-                     <div class="thumb image" style="background-image: url(${article.image});"></div>
-                     <article>
-                        <h1 class="title">${article.title}</h1>
-                        <span class="source">${article.source.name}</span>
-                     </article>
-                  </a>
+         //       <div class="item-1 ${idx}">
+         //          <a href=${article.url} class="card url">
+         //             <div class="thumb image" style="background-image: url(${article.image});"></div>
+         //             <article>
+         //                <h1 class="title">${article.title}</h1>
+         //                <span class="source">${article.source.name}</span>
+         //             </article>
+         //          </a>
          
-            `;
+         //    `;
 
-            newsList.innerHTML += content;
-         });
+         //    newsList.innerHTML += content;
+         // });
 
 
 
