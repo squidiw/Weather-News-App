723eae090ef3f9aa4156b93678b01447b37fb86f
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-02-08 15:28:39 +0000

    title, source, description & image populated from api.

--- js/news.js
+++ js/news.js
@@ -2,9 +2,8 @@ const title = document.querySelectorAll(".title"),
    image = document.querySelectorAll("div.thumb"),
    source = document.querySelectorAll("span.source"),
    url = document.querySelectorAll("a.url"),
-   description = document.querySelectorAll('.description')
+   description = document.querySelectorAll("p.description");
 
-const news = {};
 const key = "a7ca8e7a595974206e04ca91c5459b76";
 
 function getNews() {
@@ -18,15 +17,38 @@ function getNews() {
       .then(function (data) {
          // console.log(data);
          let x = 0;
-         data.articles.forEach(article => {
+         data.articles.forEach((article) => {
             title[x].innerHTML = article.title;
-            image[x].style.backgroundImage = article.image;
+            image[x].style.backgroundImage = `url(${article.image})`;
             source[x].innerHTML = article.source.name;
-            url[x].setAttribute('href', article.url);
-            url[x].setAttribute('target', '_blank');
-            description[x].innerHTML = article.description;
-             x++;
+            url[x].setAttribute("href", article.url);
+            url[x].setAttribute("target", "_blank");
+
+            x++;
          });
+
+         // data.articles.forEach( article => {
+         //    let header = document.createElement('header');
+         //    let band = document.createElement('div');
+         //    band.classList.add('band');
+         //    let card = document.createElement('a');
+         //    card.setAttribute('href', article.url);
+         //    card.classList.add('card');
+         //    let bgImg = document.createElement('div');
+         //    bgImg.classList.add('thumb');
+         //    bgImg.style.backgroundImage = `url(${article.image})`;
+         //    let artEl = document.createElement('article');
+         //    let h1 = document.createElement('h1');
+         //    h1.classList.add('title');
+         //    h1.innerHTML= article.title;
+         //    let span = document.createElement('span');
+         //    span.classList.add('source');
+         //    span.innerHTML = article.source.name;
+
+         //    band.append(card,bgImg,artEl,h1,span);
+         //    newsList.append(band);
+
+         // })
 
          // data.articles.forEach((article) => {
          //    title.innerHTML = article.title;
@@ -47,6 +69,16 @@ function getNews() {
 
          // })
       });
+   // .then(function () {
+   //    displayNews();
+   // });
 }
+
+// function displayNews() {
+//    title.innerHTML = news.title;
+//    image.style.backgroundImage = `url(${news.image})`;
+//    source.innerHTML = news.source;
+//    url.innerHTML = document.querySelector('.url').href = news.url;
+// }
 
 getNews();
