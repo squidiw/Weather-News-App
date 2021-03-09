f37e845b467ee02f0f38aa99ba4023e319f0e807
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-03-09 15:12:42 +0000

    added unsplash api to change header background images

--- js/bg_change.js
+++ js/bg_change.js
@@ -1,23 +1,39 @@
-let header = document.querySelector('header');
+// let header = document.querySelector('header');
 
-let images = [
-    "url(imgs/1.jpg)",
-    "url(imgs/2.jpg)",
-    "url(imgs/3.jpg)",
-    "url(imgs/4.jpg)",
-    "url(imgs/5.jpg)",
-    "url(imgs/6.jpg)",
-    "url(imgs/7.jpg)",
-    "url(imgs/8.jpg)",
-    "url(imgs/9.jpg)",
-    "url(imgs/10.jpg)",
-    "url(imgs/11.jpg)",
-    "url(imgs/12.jpg)"
-]
+// let images = [
+//     "url(imgs/1.jpg)",
+//     "url(imgs/2.jpg)",
+//     "url(imgs/3.jpg)",
+//     "url(imgs/4.jpg)",
+//     "url(imgs/5.jpg)",
+//     "url(imgs/6.jpg)",
+//     "url(imgs/7.jpg)",
+//     "url(imgs/8.jpg)",
+//     "url(imgs/9.jpg)",
+//     "url(imgs/10.jpg)",
+//     "url(imgs/11.jpg)",
+//     "url(imgs/12.jpg)"
+// ]
 
-setInterval(function(){
-    let bg = images[Math.floor(Math.random() * images.length ) ];
-    header.style.background = bg;
-    header.style.backgroundSize = 'cover';
-    header.style.backgroundRepeat = 'no-repeat';
-}, 5000);
+// setInterval(function(){
+//     let bg = images[Math.floor(Math.random() * images.length ) ];
+//     header.style.background = bg;
+//     header.style.backgroundSize = 'cover';
+//     header.style.backgroundRepeat = 'no-repeat';
+// }, 5000);
+
+const headerEl = document.querySelector("header");
+function renderItem() {
+   let api = `https://source.unsplash.com/1400x900/?morning,water`;
+   fetch(api).then((response) => {
+    //    console.log(response)
+      headerEl.style.background = `url(${response.url})`;
+   });
+}
+
+setInterval( function () {
+    
+      renderItem();
+    
+},5000)
+renderItem()
